282889650982769594ea4fd3ccb2ac6cf785a524
DaShaun <dashaun@dashaun.com>
2023-04-12 09:21:08 -0400

    3 instances per region

--- terraform/azurerm_spring_cloud_build_deployment.tf
+++ terraform/azurerm_spring_cloud_build_deployment.tf
@@ -2,7 +2,7 @@ resource "azurerm_spring_cloud_build_deployment" "location1" {
   name                = var.identifier
   spring_cloud_app_id = azurerm_spring_cloud_app.location1.id
   build_result_id     = "<default>"
-  instance_count      = 1
+  instance_count      = 3
   environment_variables = {
     "MY_REGION" : var.location1
   }
@@ -23,7 +23,7 @@ resource "azurerm_spring_cloud_build_deployment" "location2" {
   name                = var.identifier
   spring_cloud_app_id = azurerm_spring_cloud_app.location2.id
   build_result_id     = "<default>"
-  instance_count      = 1
+  instance_count      = 3
   environment_variables = {
     "MY_REGION" : var.location2
   }
